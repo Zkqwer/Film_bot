ee7aaa98447e3b5bd926ecfafdfe60bc73702602
Zkqwer <117681609+Zkqwer@users.noreply.github.com>
2024-04-17 21:05:13 +0400

    Add files via upload
    main update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
-from config1 import TELEGRAM_API, KINOPOISK_API
+from config import TELEGRAM_API, KINOPOISK_API
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from kinopoisk_unofficial.kinopoisk_api_client import KinopoiskApiClient
 from kinopoisk_unofficial.request.films.search_by_keyword_request import SearchByKeywordRequest
 from kinopoisk_unofficial.request.films.film_request import FilmRequest
@@ -29,29 +30,32 @@ async def send_help(message):
              'фильм - возможно такого фильма нет в базе, либо он еще не вышел\n')
 
 
-def xuy(data):
+async def film_info(message):
+    film_id = api_client.films.send_search_by_keyword_request(SearchByKeywordRequest(message.text)).films[0].film_id
+    data = api_client.films.send_film_request(FilmRequest(film_id)).film
+    inline_keyboard = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Смотреть фильм',
+                                                                                 url=f'https://www.SSpoisk.ru/film/{data.kinopoisk_id}'))
     if data.serial:
-        return f'Длина серии: {data.film_length}мин'
+        length = f'Длина серии: {data.film_length} мин'
     else:
-        return f'Длина фильма: {data.film_length}мин'
+        length = f'Длина фильма: {data.film_length} мин'
+    await message.answer_photo(photo=data.poster_url_preview,
+                               caption=f'<b>{data.name_ru} - {data.name_original} {data.rating_age_limits[3::]}+ ({data.countries[0].country})</b>\n'
+                                       f'• Дата выхода: {data.year}\n'
+                                       f'• {length}\n'
+                                       f'• Рейтинги:\n   '
+                                       f'Кинопоиск {data.rating_kinopoisk}/10\n   '
+                                       f'IMDB {data.rating_imdb}/10\n'
+                                       f'• Описание: {data.short_description}',
+                               parse_mode="HTML",
+                               reply_markup=inline_keyboard)
 
 
 @dp.message_handler(content_types=['text'])
 async def text(message: types.Message):
     try:
-        filmid = api_client.films.send_search_by_keyword_request(SearchByKeywordRequest(message.text)).films[0].film_id
-        data = api_client.films.send_film_request(FilmRequest(filmid)).film
-        await message.answer_photo(photo=data.poster_url_preview,
-                                   caption=f'<b>{data.name_ru} - {data.name_original} {data.rating_age_limits[3::]}+ ({data.countries[0].country})</b>\n'
-                                           f'• Дата выхода: {data.year}г\n'
-                                           f'• {xuy(data)}\n'
-                                           f'• Рейтинги:\n  '
-                                           f'Кинопоиск {data.rating_kinopoisk}/10\n  '
-                                           f'IMDB {data.rating_imdb}/10\n'
-                                           f'• Описание: {data.short_description}\n\n'
-                                           f'Ссылка на фильм: https://www.SSpoisk.ru/film/{data.kinopoisk_id}\n',
-                                   parse_mode="HTML")
-    except:
+        await film_info(message)
+    except IndexError:
         await message.answer(text='К сожалению, я не знаю такого фильма')
 
 
